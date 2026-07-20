09e587c3b13dc14cbb7bdc4f3e02807d9225c6fd
stealth1710 <adnan.ab164@gmail.com>
2026-07-20 23:10:29 +0100

    what is an array

---
+++ Array.js
@@ -0,0 +1,15 @@
+const array = [1,2,3,'hello']
+
+// for(const items of array){
+//     console.log(items);
+    
+// }
+
+
+array.forEach((items)=>console.log(items)
+);
+
+const newArray = array.map((items)=> 5 + items)
+
+console.log(newArray
+);
