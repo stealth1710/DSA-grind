c8ca50c29f41f749f892617cd360a01c5a431802
stealth1710 <adnan.ab164@gmail.com>
2026-07-22 14:39:17 +0100

    double linked list and hash tables

---
+++ doublLinkedList.js
@@ -0,0 +1,4 @@
+// this is a much efective way of dealing with singly linked list but with the expense of more space
+
+// each node has a next pointer and a previous pointer which is in opposite directions to make the opertions easier
+
---
+++ hashTable.js
@@ -0,0 +1,8 @@
+//also called hash maps (important)
+
+//hash table stores data as key value pairs
+
+//hash table has a hashing func. which maps the key to numbers and store it in numeric indexes
+
+//hash table implementation
+
