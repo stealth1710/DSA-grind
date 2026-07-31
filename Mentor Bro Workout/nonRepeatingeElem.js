0c2e7ea107d092e3c0ce9ed6427aad63f63dbfc0
stealth1710 <adnan.ab164@gmail.com>
2026-07-31 23:17:18 +0100

    commented the dsa code

--- Mentor Bro Workout/nonRepeatingeElem.js
+++ Mentor Bro Workout/nonRepeatingeElem.js
@@ -1,14 +1,15 @@
 function firstNonRepeatingElem(array){
-    const visited = new Map();
+    const visited = new Map(); //storing the elements and their counts in a map ds
 
-    for(const num of array){
-        if(visited.has(num)){
-            visited.set(num,visited.get(num) + 1)
+    for(const num of array){ //using for of loop as to extract the values of the contained in the array rather than their indexes
+        if(visited.has(num)){ //if the key/number in the array is already existing, update the count 
+            visited.set(num,visited.get(num) + 1) 
         }
         else{
-            visited.set(num,1)
+            visited.set(num,1) //setting the value in the ds as key and the count as their value
         }
     }
+    //looping through the array once more to get the first element in the array
 
     for(const num of array){
         if(visited.get(num) === 1){
@@ -16,6 +17,7 @@ function firstNonRepeatingElem(array){
         }
     }
 }
+//
 
 const array = [2,2,9,8,0];
 
